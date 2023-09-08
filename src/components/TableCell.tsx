fefda917865ce673648be566852fdf964d65aed7
Max Skorokhodov <skormaksim@mail.ru>
2023-09-08 15:19:36 +0300

    feat: типизовать TableCell

--- src/components/TableCell.tsx
+++ src/components/TableCell.tsx
@@ -1,6 +1,25 @@
 import { useEffect, useState } from 'react';
+import { Table, Row, Column, RowData } from '@tanstack/react-table';
 
-export const TableCell = ({ getValue, row, column, table }) => {
+declare module '@tanstack/react-table' {
+	interface TableMeta<TData extends RowData> {
+		updateData: (rowIndex: number, columnId: string, value: string) => void;
+	}
+}
+
+interface TableCellProps<TData> {
+	getValue: () => any;
+	row: Row<TData>;
+	column: Column<TData>;
+	table: Table<TData>;
+}
+
+function TableCell<TData>({
+	getValue,
+	row,
+	column,
+	table,
+}: TableCellProps<TData>) {
 	const initialValue = getValue();
 	const [value, setValue] = useState(initialValue);
 
@@ -14,9 +33,12 @@ export const TableCell = ({ getValue, row, column, table }) => {
 
 	return (
 		<input
-			style={{width: '20px'}}
+			style={{ width: '20px' }}
 			value={value}
 			onChange={e => setValue(e.target.value)}
-			onBlur={onBlur} />
+			onBlur={onBlur}
+		/>
 	);
-};
+}
+
+export default TableCell;
